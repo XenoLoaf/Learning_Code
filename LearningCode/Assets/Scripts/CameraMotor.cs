c96f84c81449e98d473d7c27c50508ab23208220
TheKidGinger <gamermakesgames@gmail.com>
2022-04-16 01:34:26 -0400

    AnotherSlightUpdate
    restarting pc and need a backup

---
+++ LearningCode/Assets/Scripts/CameraMotor.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    private void Update()
+    {
+        //follow player
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z);
+    }
+}
