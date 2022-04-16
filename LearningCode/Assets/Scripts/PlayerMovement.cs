20a48986ee9d2cc4315f5691c10328dd955cd906
TheKidGinger <gamermakesgames@gmail.com>
2022-04-16 16:29:05 -0400

    fixedMovementAgain
    Feels better, momentum is better whenever you jump, you dont stop when you hit max speed, you start at max speed

--- LearningCode/Assets/Scripts/PlayerMovement.cs
+++ LearningCode/Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-   
-    public float ramp;
     public float moveSpeed;
-    public float maxSpeed;
     public float JumpHeight;
 
     public bool isGrounded;
     public float RayCastLength;
    
-    private Rigidbody2D rb;
-    private float moveDirection;
-    private CircleCollider2D circleCollider;
+    Rigidbody2D rb;
+    public float moveDirection;
+    CircleCollider2D circleCollider;
 
     public LayerMask ground;
 
@@ -31,25 +28,20 @@ public class PlayerMovement : MonoBehaviour
         Jump();
     }
 
-    void FixedUpdate(){
-        moveDirection = Input.GetAxis("Horizontal");
-
-        if(moveDirection != 1)
-            if(moveDirection != -1)
-                moveSpeed = 1;
-                rb.velocity = new Vector2(0, rb.velocity.y);
-
-        if(moveDirection != 0) 
-            if(moveSpeed <= maxSpeed){
-               rb.velocity = new Vector2(moveDirection * (moveSpeed += ramp*Time.deltaTime), rb.velocity.y); 
-            }
+    void FixedUpdate() 
+    {
+        moveDirection = Input.GetAxisRaw("Horizontal");
+        if(moveDirection > 0.1f || moveDirection < -0.1f)
+        {
+            rb.AddForce(new Vector2 (moveDirection * moveSpeed, 0), ForceMode2D.Impulse);
         }
+    }
 
     void Jump()
     {
         if(isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector2(0, JumpHeight);
+            rb.velocity = new Vector2(rb.velocity.x, JumpHeight);
         }
         else
         {
