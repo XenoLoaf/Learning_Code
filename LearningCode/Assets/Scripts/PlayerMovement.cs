e628e0e9e219feea3d7f85bba5abd6cb9c30b8d3
TheKidGinger <gamermakesgames@gmail.com>
2022-04-16 01:13:24 -0400

    MovementMechanics Update!
    Jumping ability and ground detection

--- LearningCode/Assets/Scripts/PlayerMovement.cs
+++ LearningCode/Assets/Scripts/PlayerMovement.cs
@@ -8,18 +8,27 @@ public class PlayerMovement : MonoBehaviour
     public float ramp;
     public float moveSpeed;
     public float maxSpeed;
+    public float JumpHeight;
+
+    public bool isGrounded;
+    public float RayCastLength;
    
     private Rigidbody2D rb;
     private float moveDirection;
+    private CircleCollider2D circleCollider;
+
+    public LayerMask ground;
 
     void Awake()
     {
+        circleCollider = GetComponent<CircleCollider2D>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        GroundDetection();
+        Jump();
     }
 
     void FixedUpdate(){
@@ -35,4 +44,29 @@ public class PlayerMovement : MonoBehaviour
                rb.velocity = new Vector2(moveDirection * (moveSpeed += ramp*Time.deltaTime), rb.velocity.y); 
             }
         }
+
+    void Jump()
+    {
+        if(isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(0, JumpHeight);
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    void GroundDetection()
+    {
+        Debug.DrawRay(transform.position, Vector2.down * RayCastLength, Color.red);
+        if(Physics2D.Raycast(transform.position, Vector2.down, RayCastLength, ground))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
 }
