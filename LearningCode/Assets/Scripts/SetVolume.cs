eb9bd5b5892e02b75bcf1397b229f2d095b8b922
XenoLoaf <XenoLoaf@gmail.com>
2022-04-30 15:14:45 -0500

    .

--- LearningCode/Assets/Scripts/SetVolume.cs
+++ LearningCode/Assets/Scripts/SetVolume.cs
@@ -2,13 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SetVolume : MonoBehaviour {
 
     public AudioMixer mixer;
+    public Slider mainSlider;
+
+
+    public void Start()
+    {
+        //Adds a listener to the main slider and invokes a method when the value changes.
+        mainSlider.onValueChanged.AddListener(delegate {ValueChangeCheck(); });
+    }
+
+    // Invoked when the value of the slider changes.
+    public void ValueChangeCheck()
+    {
+        Debug.Log(mainSlider.value);
+    }
 
     public void SetLevel (float sliderValue)
-    
     {
         mixer.SetFloat ("MusicVolume", Mathf.Log10 (sliderValue) * 20 );
     }
